af1bdaf02fef39a3c6d193b721db0e55ef1d97d8
SeongHo-C <leeseong010@naver.com>
2022-07-05 22:49:13 +0900

    App 컴포넌트 만들기(State Up)

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,46 @@
 import './app.css';
 import Habits from './components/habits';
+import React, { Component } from 'react';
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Swimming', count: 0 },
+      { id: 2, name: 'Reading', count: 0 },
+      { id: 3, name: 'Hiking', count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    // 배열의 껍데기만 새로운 참조값
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+  render() {
+    return (
+      <Habits
+        habits={this.state.habits}
+        onIncrement={this.handleIncrement}
+        onDecrement={this.handleDecrement}
+        onDelete={this.handleDelete}
+      />
+    );
+  }
 }
 
 export default App;
